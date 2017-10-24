95f8e679831343baf30af39395e3fbd901bdc513
RevivoZ <hadar2703@gmail.com>
2017-10-25 01:03:25 +0300

    #3

--- app.js
+++ app.js
@@ -33,13 +33,15 @@ function Circ(_nick, _id, _radi, _team) {
 }
 
 // Arrow Constractor
-function Arrow(_x, _y) {
+function Arrow(_x, _y, _id, _speed) {
 
+	this.id = _id;
 	this.x = _x;
 	this.y = _y;
 	this.vx = 0;
 	this.vy = 0;
 	this.radius = 5;
+	this.speed = _speed ? _speed : 5;
 	this.color = 'black';
 
 }
@@ -47,7 +49,7 @@ function Arrow(_x, _y) {
 
 
 // Get Direction For Arrow Shooting
-function getDirection(playerX, playerY, mouseX, mouseY) {
+function getDirection(playerX, playerY, mouseX, mouseY, _speed) {
 
 	var dircX = mouseX - playerX;
 	var dircY = mouseY - playerY;
@@ -55,8 +57,8 @@ function getDirection(playerX, playerY, mouseX, mouseY) {
 	var splitX = (dircY / Math.abs(dircX));
 	var splitY = (dircX / Math.abs(dircY));
 
-	var totX = 5 / (Math.abs(splitX) + 1);
-	var totY = 5 / (Math.abs(splitY) + 1);
+	var totX = _speed / (Math.abs(splitX) + 1);
+	var totY = _speed / (Math.abs(splitY) + 1);
 
 	var vx = totX * (dircX / Math.abs(dircX));
 	var vy = totY * (dircY / Math.abs(dircY));
@@ -66,18 +68,48 @@ function getDirection(playerX, playerY, mouseX, mouseY) {
 
 
 
-/**** The Main Loop For All Objects Movement ****/
+/************ The Main Loop For All Objects Movement ***********/
 function loop() {
 
 	for (i = 0; i < users.length; i++) {
+
+		// Players Movement
 		users[i].x += users[i].vx;
 		users[i].y += users[i].vy;
 
+		// Arrows Movement
 		for (j = 0; j < users[i].arrows.length; j++) {
 			users[i].arrows[j].x += users[i].arrows[j].vx;
 			users[i].arrows[j].y += users[i].arrows[j].vy;
+
+			if (users[i].arrows[j].x < 0 || users[i].arrows[j].x > canvWidth) {
+				users[i].arrows.splice(j, 1);
+				break;
+			}
+			if (users[i].arrows[j].y < 0 || users[i].arrows[j].y > canvHeight) {
+				users[i].arrows.splice(j, 1);
+				break;
+			}
+
+
+			/*************** Hit On Player Check ****************/
+			for (z = 0; z < users.length; z++) {
+				if (Math.abs(users[z].x - users[i].arrows[j].x) <= users[i].arrows[j].radius * 2) {
+					if (Math.abs(users[z].y - users[i].arrows[j].y) <= users[i].arrows[j].radius * 2) {
+						if (users[i].arrows[j].id != users[z].id) {
+							users[i].arrows.splice(j, 1);
+							break;
+						}
+					}
+				}
+			}
+			/*****************************************************/
+
+
+
 		}
 	}
+
 
 	io.sockets.emit('update', users);
 }
@@ -96,7 +128,6 @@ io.sockets.on('connection', function (socket) {
 	// Create A New User
 	socket.on('new user', function (data) {
 		users.push(new Circ(data, socket.id, 10, (users.length % 2 == 0)));
-		console.log(users);
 		if (users.length % 2 == 0 && users.length > 0) {
 			io.sockets.emit('update', users);
 		}
@@ -119,9 +150,9 @@ io.sockets.on('connection', function (socket) {
 	socket.on('shoot', function (data) {
 		for (i = 0; i < users.length; i++) {
 			if (socket.id == users[i].id) {
-				var arrow = new Arrow(users[i].x, users[i].y);
+				var arrow = new Arrow(users[i].x, users[i].y, users[i].id);
 
-				var direc = getDirection(users[i].x, users[i].y, data.x, data.y);
+				var direc = getDirection(users[i].x, users[i].y, data.x, data.y, arrow.speed);
 
 				arrow.vx = direc[0];
 				arrow.vy = direc[1];
